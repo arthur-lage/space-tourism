f393f08c270aa3bdcbc0e051557e56c102b39bdf
Arthur Lage <arthurlage2006@gmail.com>
2021-11-02 23:47:57 -0300

    mobile menu added, but destination, crew and technology still need fixing in their responsivity

--- starter-code/scripts/menu.js
+++ starter-code/scripts/menu.js
@@ -1,18 +1,18 @@
 const hamburger = document.querySelector(".hamburger")
 const hamburgerImage = document.querySelector(".hamburger img")
+const mobileNavbar = document.querySelector(".nav-mobile")
 
 let menuIsOpen = false
 
 hamburger.addEventListener("click", () => {
-    console.log(menuIsOpen)
-
     if(menuIsOpen == false){
         menuIsOpen = true
         hamburgerImage.src = './assets/shared/icon-close.svg'
+        mobileNavbar.classList.add("active")
+        
     } else {
         menuIsOpen = false
         hamburgerImage.src = './assets/shared/icon-hamburger.svg'
+        mobileNavbar.classList.remove("active")
     }
-
-    console.log(menuIsOpen)
 })
